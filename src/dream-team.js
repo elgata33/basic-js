f2fdf7af1be74e623090182c02bff69fcd450fd7
elgata33 <garashov123@gmail.com>
2021-03-29 21:28:28 +0300

    feat: finish 6/10 tasks

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+  if (!Array.isArray(arr)) return false;
+  if (!arr) return;
+  let teamName = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === 'string') {
+      teamName.push(arr[i].trim().charAt(0).toUpperCase());
+    }
+  }
+  return teamName.sort().join('');
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,35 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw Error;
+  }
+
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--double-next' && i !== arr.length - 1) {
+      newArr.push(arr[i + 1]);
+    } else if (arr[i] === '--double-prev' && i > 0) {
+      newArr.push(newArr[newArr.length - 1]);
+    } else if (arr[i] === '--discard-next' && i !== arr.length - 1) {
+      newArr.push(undefined);
+      i++;
+    } else if (arr[i] === '--discard-prev' && i > 0) {
+      newArr.pop();
+    } else if (
+      arr[i] !== '--double-next' &&
+      arr[i] !== '--double-prev' &&
+      arr[i] !== '--discard-next' &&
+      arr[i] !== '--discard-prev'
+    ) {
+      newArr.push(arr[i]);
+    }
+  }
+
+  let result = newArr.filter(function (el) {
+    return el !== undefined;
+  });
+
+  return result;
 };
